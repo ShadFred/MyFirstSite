c79a6f36f21016fbcf85342e156fce6b81f069bc
ShadFred <gvkashin@gmail.com>
2025-06-23 01:29:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+//cd C:\Users\gvkas\OneDrive\Документы\MyFirstSite\ (Для быстрой проверки)
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Geeks!</h1><p>This is your simple Express server.</p>');
 });
